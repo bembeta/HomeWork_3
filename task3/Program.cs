775a099409007edab69166ffe02a9aca057782ac
stinqyy <maxvik19m@mail.ru>
2023-01-21 18:49:06 +0300

    Выполнил третие задание

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Напишите программу, которая принимает на вход число (N)
+    и выдаёт таблицу кубов чисел от 1 до N.
+ */
+
+int GetNumber()
+{
+Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number; 
+}
+int number = GetNumber();
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
+
